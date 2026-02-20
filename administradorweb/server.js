6b72ccf59b04444d22ed2aa736a6a0aa8cbdc45e
Midel9724 <arelo1357924@email.com>
2026-02-20 16:34:42 -0600

    Finalizando configuración de base de datos y funciones de app.js

--- administradorweb/server.js
+++ administradorweb/server.js
@@ -136,4 +136,7 @@ app.delete('/eliminar/:tabla/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Servidor Peli-Ya listo en puerto 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
